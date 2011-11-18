9a015fc208853c2cbedd5b449718fb993eb056de
Ankit Premrajka <ankitp@codeaurora.org>
2011-11-18 12:52:18 -0800

    camcorder: disable audio temporarily.
    - set property camcorder.debug.disableaudio 1 to disable audio encode
    
    Patchset 1: Add QCOM_HARDWARE ifdefs.
    
    Change-Id: I33b6ba1cfc45c83f7779ae1abe3fddb4631a309e
    Signed-off-by: Evan McClain <aeroevan@gmail.com>

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -54,6 +54,10 @@
 
 #include "ARTPWriter.h"
 
+#ifdef QCOM_HARDWARE
+#include <cutils/properties.h>
+#endif
+
 namespace android {
 
 // To collect the encoder usage for the battery app
@@ -72,7 +76,13 @@ StagefrightRecorder::StagefrightRecorder()
       mOutputFd(-1),
       mAudioSource(AUDIO_SOURCE_CNT),
       mVideoSource(VIDEO_SOURCE_LIST_END),
+#ifdef QCOM_HARDWARE
+      mStarted(false), mSurfaceMediaSource(NULL),
+      mDisableAudio(false) {
+#else
       mStarted(false), mSurfaceMediaSource(NULL) {
+#endif
+
 
     ALOGV("Constructor");
     reset();
@@ -103,6 +113,12 @@ status_t StagefrightRecorder::setAudioSource(audio_source_t as) {
         ALOGE("Invalid audio source: %d", as);
         return BAD_VALUE;
     }
+
+#ifdef QCOM_HARDWARE
+    if (mDisableAudio) {
+        return OK;
+    }
+#endif
 
     if (as == AUDIO_SOURCE_DEFAULT) {
         mAudioSource = AUDIO_SOURCE_MIC;
@@ -154,6 +170,12 @@ status_t StagefrightRecorder::setAudioEncoder(audio_encoder ae) {
         ALOGE("Invalid audio encoder: %d", ae);
         return BAD_VALUE;
     }
+
+#ifdef QCOM_HARDWARE
+    if (mDisableAudio) {
+        return OK;
+    }
+#endif
 
     if (ae == AUDIO_ENCODER_DEFAULT) {
         mAudioEncoder = AUDIO_ENCODER_AMR_NB;
@@ -1812,6 +1834,13 @@ status_t StagefrightRecorder::reset() {
     mLongitudex10000 = -3600000;
 
     mOutputFd = -1;
+
+#ifdef QCOM_HARDWARE
+    // Disable Audio Encoding
+    char value[PROPERTY_VALUE_MAX];
+    property_get("camcorder.debug.disableaudio", value, "0");
+    if(atoi(value)) mDisableAudio = true;
+#endif
 
     return OK;
 }
