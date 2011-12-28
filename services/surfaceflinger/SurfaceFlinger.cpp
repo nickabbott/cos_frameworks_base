4c45bc42df978dfe4cf64c5dade051dc68bafce3
Prabhanjan Kandula <pkandula@codeaurora.org>
2011-12-28 18:59:57 +0530

    frameworks/base: fix delay in HDMI update
    SF threadloop now checks for dirtyregion and returns
    if it is empty. Mark dirtyregion as nonempty so that
    threadloop actually executes when required
    
    Change-Id: I00e132433922509468b258c33279c819b3294d73
    CRs-fixed: 327432

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -1393,6 +1393,7 @@ void SurfaceFlinger::updateHwcExternalDisplay(int externaltype)
 {
     invalidateHwcGeometry();
     const DisplayHardware& hw(graphicPlane(0).displayHardware());
+    mDirtyRegion.set(hw.bounds());
     HWComposer& hwc(hw.getHwComposer());
     hwc.enableHDMIOutput(externaltype);
 }
