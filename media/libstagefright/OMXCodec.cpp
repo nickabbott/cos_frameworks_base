1515623542e334f1249e73eee863c652105b68ac
Vinay Kalia <vkalia@codeaurora.org>
2012-04-02 11:32:24 -0700

    libstagefright: Fixes a NULL pointer exception.
    Fixes a NULL pointer exception incase of secure
    playback. This exception is only in a log message
    so it happens only when debug messages are enabled.
    
    Change-Id: Iee5ca0708c7bd8c4388e93df20a928224575faf1

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -3307,10 +3307,6 @@ bool OMXCodec::drainInputBuffer(BufferInfo *info) {
         mNoMoreOutputData = false;
     }
 
-    CODEC_LOGV("Calling emptyBuffer on buffer %p (length %d), "
-               "timestamp %lld us (%.2f secs)",
-               info->mBuffer, offset,
-               timestampUs, timestampUs / 1E6);
 
     if (info == NULL) {
         CHECK(mFlags & kUseSecureInputBuffers);
@@ -3321,6 +3317,11 @@ bool OMXCodec::drainInputBuffer(BufferInfo *info) {
         // to use it to signal EOS to the codec.
         info = findEmptyInputBuffer();
     }
+
+    CODEC_LOGV("Calling emptyBuffer on buffer %p (length %d), "
+               "timestamp %lld us (%.2f secs)",
+               info->mBuffer, offset,
+               timestampUs, timestampUs / 1E6);
 
     err = mOMX->emptyBuffer(
             mNode, info->mBuffer, 0, offset,
