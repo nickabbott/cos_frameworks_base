3317fd43d4628be6c0784ef4c8c826995d39bbc2
Steve Kondik <shade@chemlab.org>
2012-05-28 07:19:59 -0700

    systemui: Add option to enable notification counts
     * The code is already there, this adds a runtime toggle for it.
    
    Change-Id: I093fae9b0e2e263c11a41f07c9669c18afda978d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2294,6 +2294,12 @@ public final class Settings {
 		/* END Constants for Theme compatibility */
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
