10fc46937b48c03a99eece82ea2c074fb3941f31
Rick C <rickfitsnugly@gmail.com>
2012-05-28 17:47:17 -0400

    Lockscreen weather: option to invert low/high temp display (1/2)
    Change-Id: Ie693e3ab0b52d3034b516635c45dba4730b61160
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2238,6 +2238,12 @@ public final class Settings {
           */
           public static final String STATUSBAR_BATT_STYLE = "statusbar_batt_style";
 
+        /*         
+         * Invert low/high temperature display
+         * @hide
+         */
+        public static final String WEATHER_INVERT_LOWHIGH = "weather_invert_lowhigh";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
